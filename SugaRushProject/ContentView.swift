33490a00611fb8da697b7d00a7aa79bc2cf9d1bc
Lucas Busetto <lbusetto99@live.com>
2024-11-27 23:04:06 -0500

    Initial Commit

---
+++ SugaRushProject/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SugaRushProject
+//
+//  Created by Lucas Busetto on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("SugaRush")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SugaRushProject/SugaRushProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugaRushProjectApp.swift
+//  SugaRushProject
+//
+//  Created by Lucas Busetto on 2024-11-27.
+//
+
+import SwiftUI
+
+@main
+struct SugaRushProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
