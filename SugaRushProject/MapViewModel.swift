5cf5ceb51a87ea1f7f554b15d8db2afe0f2b88c1
davidCaluag <caluagd@sheridancollege.ca>
2024-12-08 02:50:30 -0500

    Added map and voice

---
+++ SugaRushProject/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  SugaRushProject
+//
+//  Created by David C on 2024-12-08.
+//
+
+import Foundation
---
+++ SugaRushProject/SpeechToTextViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechToTextViewModel.swift
+//  SugaRushProject
+//
+//  Created by David C on 2024-12-08.
+//
+
+import Foundation
