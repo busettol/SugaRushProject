b23eab3b91b9c3b9cd3733b17eb49dac769965f2
Lucas Busetto <lbusetto99@live.com>
2024-12-09 14:45:34 -0500

    Added firebase and account creation/sign in

---
+++ SugaRushProject/Views/AccountCreation.swift
@@ -0,0 +1,18 @@
+//
+//  AccountCreation.swift
+//  SugaRushProject
+//
+//  Created by Lucas Busetto on 2024-12-09.
+//
+
+import SwiftUI
+
+struct AccountCreation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountCreation()
+}
