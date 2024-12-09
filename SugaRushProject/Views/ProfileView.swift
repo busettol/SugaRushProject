6e6419a8f8abf91e6770c3ef4d91c486325323e8
Lucas Busetto <lbusetto99@live.com>
2024-12-09 11:51:31 -0500

    test

--- SugaRushProject/Views/ProfileView.swift
+++ SugaRushProject/Views/ProfileView.swift
@@ -11,6 +11,7 @@ struct ProfileView: View {
     @State var col1 = Color(red: 255/255, green: 136/255, blue: 233/255)
     @State var col2 = Color(red: 249/255, green: 195/255, blue: 195/255)
     
+    
     var body: some View {
         ZStack{
             LinearGradient(colors: [col1, col2], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
