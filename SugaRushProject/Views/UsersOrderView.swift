8f17186be5062d76d5ed320b40547a4c08a7498b
davidCaluag <caluagd@sheridancollege.ca>
2024-12-10 16:01:53 -0500

    >Not sure if this works.
    Added viewing orders.

---
+++ SugaRushProject/Views/UsersOrderView.swift
@@ -0,0 +1,19 @@
+//
+//  UsersOrderView.swift
+//  SugaRushProject
+//
+//  Created by David C on 2024-12-10.
+//
+
+import SwiftUI
+
+struct UsersOrderView: View {
+	
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersOrderView()
+}
